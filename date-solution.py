932eed238ad8e82a57dbaf642e6d100001a3d531
Federico Capaldo <federico.capaldo1@gmail.com>
2022-07-30 03:36:14 +0100

    updated indentation and solution

--- date-solution.py
+++ date-solution.py
@@ -10,34 +10,39 @@
 #   year
 # 1.2 create a function called get_date(self) in the DateStamp class that return the date in the format dd/mm/yyyy
 #   hint: str(number) converts a number to string, you may need this to convert the day, month, year from numbers to string in order to print correctly.
-# 1.3 create a function called set_date(self, day, month, year) in the DateStamp class to set a new day, month, year in its respective properties
+# 4. create a function called set_date(self, new_day, new_month, new_year) in the DateStamp class to set a new day, month, year in its respective properties
 class DateStamp:
 
-    def __init__(self, day, month, year):
-        self.day = day
-        self.month = month
-        self.year = year
+  def __init__(self, day, month, year):
+    self.day = day
+    self.month = month
+    self.year = year
 
-    def get_date(self,):
-        return str(self.day) + "/" + str(self.month) + "/" + str(self.year)
+  def get_date(self):
+    if self.month < 10:
+      actual_month = "0" + str(self.month)
+    else:
+      actual_month = str(self.month)
+    return str(self.day) + "/" + actual_month + "/" + str(self.year)
 
-    def set_date(self, new_day, new_month, new_year):
-        self.day = new_day
-        self.month = new_month
-        self.year = new_year
+  def set_date(self, new_day, new_month, new_year):
+    self.day = new_day
+    self.month = new_month
+    self.year = new_year
+
 
 if __name__ == "__main__":
-    print("hello, world!")
+  print("hello, world!")
 
-    # 2. create a DateStamp object called dateOne
-    #  (initialize the object with day, month, year of your choice)
-    dateOne = DateStamp(18, 7, 1980)
+  # 2. create a DateStamp object called dateOne
+  #  (initialize the object with day, month, year of your choice)
+  dateOne = DateStamp(30, 7, 2022)
 
-    # 3. print the date of dateOne
-    print(dateOne.get_date())
+  # 3. print the date of dateOne
+  print(dateOne.get_date())
 
-    # 4. give dateOne a new day, month, year
-    dateOne.set_date(25, 12, 2022)
+  # 5. give dateOne a new day, month, year
+  dateOne.set_date(15, 10, 2022)
 
-    # 5. print again the date of dateOne
-    print(dateOne.get_date())
+  # 6. print again the date of dateOne
+  print(dateOne.get_date())
